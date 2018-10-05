9b9265147079ab41a7592013555036f16267c555
yogesh.s <yogesh.s@aux.go-jek.com>
2018-10-05 08:51:00 +0530

    Added simple Employee

--- employee_project/employee/models.py
+++ employee_project/employee/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+    	return self.name
+
