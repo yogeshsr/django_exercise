d74a1c79e1e67161edebd4a9ca19eda3280e1c28
yogesh.s <yogesh.s@aux.go-jek.com>
2018-10-07 13:00:34 +0530

    Adding Employee index view

--- employee_project/employee/views.py
+++ employee_project/employee/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Employee
+def index(request):
+    # return HttpResponse("Hello, world. You're at the employee index.")
+
+    employees = Employee.objects.all()
+    context = {
+    	'employees': employees
+    }
+
+    template = loader.get_template('employee/index.html')
+
+
+    return HttpResponse(template.render(context, request))
