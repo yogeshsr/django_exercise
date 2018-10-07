2c7e6a12aca8896361cdd46d9a6b66ae9a9ab920
yogesh.s <yogesh.s@aux.go-jek.com>
2018-10-07 13:00:18 +0530

    Adding models

--- employee_project/employee/admin.py
+++ employee_project/employee/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Employee
+from .models import Employee, Expense, Project
 
 admin.site.register(Employee)
+admin.site.register(Expense)
+admin.site.register(Project)
